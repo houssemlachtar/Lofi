f5e45fd73914e29b67982e6314bc3a3f7b9ddd28
Houssem Lachtar <lachtarhoussem4@gmail.com>
2022-07-05 18:00:45 +0100

    Update Pomodoro.jsx

--- src/component/modal/Pomodoro.jsx
+++ src/component/modal/Pomodoro.jsx
@@ -165,7 +165,7 @@ function SettingPomo({handleSwitch, handleClose, defaultTime, setDefaultTime, se
 
 const Stylee = {
   transform: 'scale(0.7)',
-  marginLeft: '-14rem',
+  marginLeft: '-13rem',
 };
 
 function Pomodoro() {
